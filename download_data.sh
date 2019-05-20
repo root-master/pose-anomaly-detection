22ba826910bf2deb73a4a29bb9076cf7dee5748d
Jacob Rafati <yrafati@gmail.com>
2019-05-20 11:18:42 -0700

    download data with bash

--- download_data.sh
+++ download_data.sh
@@ -1,8 +1,5 @@
 #!/bin/bash          
 
-# 
-# "https://www.dropbox.com/sh/75v5ehq4cdg5g5g/AABJtkTnNc8LcVTfH1gE_uFoa/Anomaly-Videos-Part-1.zip?dl=0"
-
 declare -a URLS=("https://www.dropbox.com/sh/75v5ehq4cdg5g5g/AACxcnr1G--t0KWdrpj1tQmpa/Anomaly_Train.txt?dl=0"
 				 "https://www.dropbox.com/sh/75v5ehq4cdg5g5g/AABJtkTnNc8LcVTfH1gE_uFoa/Anomaly-Videos-Part-1.zip?dl=0"
 				 "https://www.dropbox.com/sh/75v5ehq4cdg5g5g/AAAbdSEUox64ZLgVAntr2WgSa/Anomaly-Videos-Part-2.zip?dl=0"
@@ -16,6 +13,7 @@ declare -a URLS=("https://www.dropbox.com/sh/75v5ehq4cdg5g5g/AACxcnr1G--t0KWdrpj
 				 "https://www.dropbox.com/sh/75v5ehq4cdg5g5g/AAAHZByMMGCVms4hhHZU2pMBa/Training-Normal-Videos-Part-2.zip?dl=0"
 				 "https://www.dropbox.com/sh/75v5ehq4cdg5g5g/AAD5w76F_SZLdBgxVdvko-z5a/UCF_Crimes-Train-Test-Split.zip?dl=0")
 
+
 if [ -d "data" ]; then
 	echo "data folder already exsits!"
 else
@@ -23,21 +21,22 @@ else
 	mkdir "data"
 fi
 
-declare -a FILES=("data/Anomaly_Train.txt"
-				  "data/Anomaly-Videos-Part-1.zip"
-				  "data/Anomaly-Videos-Part-2.zip"
-				  "data/Anomaly-Videos-Part-3.zip"
-				  "data/Anomaly-Videos-Part-4.zip"
-				  "data/Normal_Videos_for_Event_Recognition.zip"
-				  "data/ReadMe-Anomaly-Detection.txt"
-				  "data/Temporal_Anomaly_Annotation_for_Testing_Videos.txt"
-				  "data/Testing_Normal_Videos.zip"
-				  "data/Training-Normal-Videos-Part-1.zip"
-				  "data/Training-Normal-Videos-Part-2.zip"
-				  "data/UCF_Crimes-Train-Test-Split.zip")
+declare -a FILENAMES=("data/Anomaly_Train.txt"
+				      "data/Anomaly-Videos-Part-1.zip"
+				  	  "data/Anomaly-Videos-Part-2.zip"
+				      "data/Anomaly-Videos-Part-3.zip"
+				      "data/Anomaly-Videos-Part-4.zip"
+				      "data/Normal_Videos_for_Event_Recognition.zip"
+				      "data/ReadMe-Anomaly-Detection.txt"
+				      "data/Temporal_Anomaly_Annotation_for_Testing_Videos.txt"
+				      "data/Testing_Normal_Videos.zip"
+				      "data/Training-Normal-Videos-Part-1.zip"
+				      "data/Training-Normal-Videos-Part-2.zip"
+				      "data/UCF_Crimes-Train-Test-Split.zip")
+
 
 
 for i in "${!URLS[@]}"
 do
-	echo "${FILES[$i]}" "${URLS[$i]}"
+	wget -O "${FILENAMES[$i]}" "${URLS[$i]}"
 done
