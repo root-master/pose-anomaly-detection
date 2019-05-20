df0ccd80c99b5492c7050fc9d429c52d10522309
Jacob Rafati <yrafati@gmail.com>
2019-05-20 11:14:26 -0700

    download data with bash

--- data/download.sh
+++ data/download.sh
@@ -1,9 +1,8 @@
 #!/bin/bash          
 
-# "https://www.dropbox.com/sh/75v5ehq4cdg5g5g/AACxcnr1G--t0KWdrpj1tQmpa/Anomaly_Train.txt?dl=0"
-# "https://www.dropbox.com/sh/75v5ehq4cdg5g5g/AABJtkTnNc8LcVTfH1gE_uFoa/Anomaly-Videos-Part-1.zip?dl=0"
-
-declare -a URLS=("https://www.dropbox.com/sh/75v5ehq4cdg5g5g/AAAbdSEUox64ZLgVAntr2WgSa/Anomaly-Videos-Part-2.zip?dl=0"
+declare -a URLS=("https://www.dropbox.com/sh/75v5ehq4cdg5g5g/AACxcnr1G--t0KWdrpj1tQmpa/Anomaly_Train.txt?dl=0"
+				 "https://www.dropbox.com/sh/75v5ehq4cdg5g5g/AABJtkTnNc8LcVTfH1gE_uFoa/Anomaly-Videos-Part-1.zip?dl=0"
+				 "https://www.dropbox.com/sh/75v5ehq4cdg5g5g/AAAbdSEUox64ZLgVAntr2WgSa/Anomaly-Videos-Part-2.zip?dl=0"
 				 "https://www.dropbox.com/sh/75v5ehq4cdg5g5g/AAAgpsRNSHI_BtRnSCxxR7j9a/Anomaly-Videos-Part-3.zip?dl=0"
 				 "https://www.dropbox.com/sh/75v5ehq4cdg5g5g/AABqY-3fJSmSMafFIlJXRE-9a/Anomaly-Videos-Part-4.zip?dl=0"
 				 "https://www.dropbox.com/sh/75v5ehq4cdg5g5g/AACyDI-0oRqqiqUcAulw_x5wa/Normal_Videos_for_Event_Recognition.zip?dl=0"
@@ -15,7 +14,29 @@ declare -a URLS=("https://www.dropbox.com/sh/75v5ehq4cdg5g5g/AAAbdSEUox64ZLgVAnt
 				 "https://www.dropbox.com/sh/75v5ehq4cdg5g5g/AAD5w76F_SZLdBgxVdvko-z5a/UCF_Crimes-Train-Test-Split.zip?dl=0")
 
 
-for URL in "${URLS[@]}"
+if [ -d "data" ]; then
+	echo "data folder already exsits!"
+else
+	echo "data folder is created."
+	mkdir "data"
+fi
+
+declare -a FILENAMES=("data/Anomaly_Train.txt"
+				      "data/Anomaly-Videos-Part-1.zip"
+				  	  "data/Anomaly-Videos-Part-2.zip"
+				      "data/Anomaly-Videos-Part-3.zip"
+				      "data/Anomaly-Videos-Part-4.zip"
+				      "data/Normal_Videos_for_Event_Recognition.zip"
+				      "data/ReadMe-Anomaly-Detection.txt"
+				      "data/Temporal_Anomaly_Annotation_for_Testing_Videos.txt"
+				      "data/Testing_Normal_Videos.zip"
+				      "data/Training-Normal-Videos-Part-1.zip"
+				      "data/Training-Normal-Videos-Part-2.zip"
+				      "data/UCF_Crimes-Train-Test-Split.zip")
+
+
+
+for i in "${!URLS[@]}"
 do
-	wget "$URL"
+	wget -O "${FILENAMES[$i]}" "${URLS[$i]}"
 done
