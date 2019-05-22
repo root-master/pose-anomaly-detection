3e907176dab62d61d6616510adf06208cf4258b0
Jacob Rafati <yrafati@gmail.com>
2019-05-22 11:14:34 -0700

    fixed an issue

--- run_alpha_pose_folder.sh
+++ run_alpha_pose_folder.sh
@@ -2,6 +2,8 @@
 
 declare FOLDER="/media/sdc1/pose-anomaly-detection/temp-ffmpeg/"
 declare RESULTS="/media/sdc1/pose-anomaly-detection/pose-features/Assault/"
-declare ALPHAPOSE_FOLDER_DEMO="demo.py"
+declare ALPHAPOSE_FOLDER_DEMO="/media/sdc1/AlphaPose/demo.py"
 
 python3 $ALPHAPOSE_FOLDER_DEMO --indir ${FOLDER} --outdir ${RESULTS}
+
+python3 demo.py --indir /media/sdc1/pose-anomaly-detection/temp-ffmpeg/ --outdir /media/sdc1/pose-anomaly-detection/pose-features/Assault/
