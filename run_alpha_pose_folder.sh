948063a4c32322f71a2d95c030a58b61b6432e32
Jacob Rafati <yrafati@gmail.com>
2019-05-22 09:42:12 -0700

    AlphaPose is not able to deal with absolute/relative path

--- run_alpha_pose_folder.sh
+++ run_alpha_pose_folder.sh
@@ -2,6 +2,6 @@
 
 declare FOLDER="/media/sdc1/pose-anomaly-detection/temp-ffmpeg/"
 declare RESULTS="/media/sdc1/pose-anomaly-detection/pose-features/Assault/"
-declare ALPHAPOSE_FOLDER_DEMO="/media/sdc1/AlphaPose/demo.py"
+declare ALPHAPOSE_FOLDER_DEMO="demo.py"
 
 python3 $ALPHAPOSE_FOLDER_DEMO --indir ${FOLDER} --outdir ${RESULTS}
